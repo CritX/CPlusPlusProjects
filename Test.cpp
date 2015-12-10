ba6e596f3d84e2398f25eb15b735f8e748f640b6
MobCraftKrazZ <mobcraft120@gmail.com>
2015-12-10 12:02:32 -0600

    Added this_thread and sleep_for

--- Test.cpp
+++ Test.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 using namespace std;
 int main(){
 cout << "This a test CPP file." << endl;
+this_thread::sleep_for(chrono::seconds(5));
+system("PAUSE");
 cout << "Lolo!" << endl;
+this_thread::sleep_for(chrono::seconds(5));
+return 0;
 }
